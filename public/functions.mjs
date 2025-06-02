425ee571202ce7ba3623b1ccb1eedf751b4fe299
AmirUhmmer <jazmirjulio@gmail.com>
2025-06-02 15:14:39 +0200

    adjusted markup layer

--- public/functions.mjs
+++ public/functions.mjs
@@ -87,9 +87,9 @@ class PencilButton extends Autodesk.Viewing.Extension {
           window.markupsExt.createMarkupSheet();
         }
         // Enter edit mode **********************************************************************************************************
-        window.markupsExt.loadMarkups(window.svgData[0].content, "markupLayer1", () => {
+        window.markupsExt.loadMarkups(window.svgData[0].content, "markups-svg", () => {
             console.log("Markup layer loaded");
-            window.markupsExt.enterEditMode("markupLayer1");
+            window.markupsExt.enterEditMode("markups-svg");
         });
 
 
